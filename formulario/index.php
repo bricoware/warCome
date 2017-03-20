37815b0e8bd55db0f11ca5da63b03da26053d9c3
Cramsi121 <cramsi121@gmail.com>
2017-03-20 13:58:30 +0100

    Modulo Formulario Nuevo Personaje
    Lo adapte a MVC

---
+++ formulario/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title> The Legend of Warcome - Nueva Partida </title>
+	</head>
+	<body>
+		<?php
+		
+			require_once('views/formularioPersonaje.php');
+			
+		?>
+	</body>
+</html>
