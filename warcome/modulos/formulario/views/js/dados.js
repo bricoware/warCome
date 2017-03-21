4b6c588c4b96610af52793bd85003639febeeee0
Cramsi121 <cramsi121@gmail.com>
2017-03-21 09:08:10 +0100

    Proyecto con un par de modulos (Resubido)

---
+++ warcome/modulos/formulario/views/js/dados.js
@@ -0,0 +1,27 @@
+function dados(tirada){
+/* tirada en formato 1d6. Primer número indica el número de dados. Segundo número indica el tipo de dado (número de caras). */
+	var d = tirada.indexOf("d");
+	
+	var numeroDados = parseInt(tirada.substring(0, d));		/* Primer número */
+	
+	var tipoDado = parseInt(tirada.substring(d+1));			/* Segundo número */
+	
+	var mostrar = document.getElementById('mostrar');
+	
+	//console.log(numeroDados);
+	//console.log(tipoDado);
+	
+	var resultadoTirada = 0;
+	
+	for (var n = 0; n < numeroDados; n++){
+		var random = parseInt((Math.random()*tipoDado) + 1);
+		if (random == tipoDado + 1){			/* Por la remota posibilidad de que Math.random() = 1 */
+			resultadoTirada += random - 1;
+		} else {
+			resultadoTirada += random;
+		}
+	}
+	mostrar.innerHTML = resultadoTirada;
+	console.log(resultadoTirada);
+	return resultadoTirada;
+}
---
+++ warcome/modulos/login/controllers/logout.php
@@ -0,0 +1,10 @@
+<?php
+
+	session_start();
+	
+	unset($_SESSION['autentificado']);
+	unset($_SESSION['usuario']);
+	
+	header("Location: http://warcome.local/index.php");
+
+?>
