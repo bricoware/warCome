f36aeff7f6e7da1f37509189ce5917581cfa0219
AnitaGonzalez13 <anita.gonz13@gmail.com>
2017-03-21 10:47:46 +0100

    seleccion de personaje
    echarle un vistazo

---
+++ warcome/modulos/login/controllers/registroUsuario.php
@@ -0,0 +1,17 @@
+<?php
+
+	if(isset($_POST['nombreRegistro']) && isset($_POST['passRegistro']) && isset($_POST['repitePass'])){
+		if( count($_POST['nombreRegistro']) > 0 && count($_POST['passRegistro']) > 0 && count($_POST['repitePass']) > 0){
+			if ($_POST['passRegistro'] == $_POST['repitePass']){
+				require_once(dirname( dirname( __FILE__) )."/model/nuevoUsuario.php");
+				$nuevoUsuario = new nuevoUsuario($_POST["nombreRegistro"], $_POST["passRegistro"]);
+				$nuevoUsuario->registraUsuario();
+			} else {
+				echo "Las contraseñas no coinciden";
+			}
+		}
+	}else{
+		echo "No se puede mostrar el contenido de este módulo";
+	}
+	
+?>
