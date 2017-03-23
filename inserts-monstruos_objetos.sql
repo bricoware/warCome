ff8fec90126974781204fbb950705fa969e42278
Cramsi121 <cramsi121@gmail.com>
2017-03-23 12:56:54 +0100

    Cambios en valores en inserts
    También añadí un objeto y 3 pociones nuevas

--- inserts-monstruos_objetos.sql
+++ inserts-monstruos_objetos.sql
@@ -1,45 +1,46 @@
 /* Insertar monstruos */
 
 INSERT INTO `monstruo` (`idMonstruo`, `nombreMonstruo`, `ataqueMonstruo`, `acMonstruo`, `vidaMonstruo`, `xpOtorgada`, `posibilidadGolpear`, `oroOtorgado`) VALUES
-(1, "KOBOLD", '1d4', 1, 30, 10, 0, 5),
-(2, "GNOLL", '1d6', 2, 35, 20, 1, 5),
-(3, "TROLL", '1d8', 3, 45, 30, 1, 5),
-(4, "GIGANTE DE LA MONTAÑA", '1d10', 4, 100, 60, 2, 10),
+(1, "KOBOLD", '1d4', 1, 30, 100, 0, 5),
+(2, "GNOLL", '1d6', 2, 35, 200, 1, 5),
+(3, "TROLL", '1d8', 3, 45, 300, 1, 5),
+(4, "GIGANTE DE LA MONTAÑA", '1d10', 4, 100, 600, 2, 10),
 
-(5, "FELHOUND (PERRO DE FUEGO)", '1d4', 1, 25, 10, 2, 10),
-(6, "GÓLEM de LAVA", '1d6', 2, 35, 20, 2, 10),
-(7, "ELEMENTAL DE FUEGO", '1d8', 3, 55, 35, 2, 10),
-(8, "HIDRA DE FUEGO", '1d10', 4, 75, 50, 2, 10),
+(5, "FELHOUND (PERRO DE FUEGO)", '1d4', 1, 25, 100, 2, 10),
+(6, "GÓLEM de LAVA", '1d6', 2, 35, 200, 2, 10),
+(7, "ELEMENTAL DE FUEGO", '1d8', 3, 55, 350, 2, 10),
+(8, "HIDRA DE FUEGO", '1d10', 4, 75, 500, 2, 10),
 
-(9, "WITCH (LA BRUJA GRITONA)", '1d4', 1, 25, 20, 2, 15),
-(10, "BANSHEE (FANTASMA)", '1d6', 2, 35, 30, 2, 15),
-(11, "JINETE FANTASMA", '1d8', 3, 60, 40, 2, 15),
-(12, "NAZGUL", '1d10', 4, 80, 50, 2, 20),
+(9, "WITCH (LA BRUJA GRITONA)", '1d4', 1, 25, 200, 2, 15),
+(10, "BANSHEE (FANTASMA)", '1d6', 2, 35, 300, 2, 15),
+(11, "JINETE FANTASMA", '1d8', 3, 60, 400, 2, 15),
+(12, "NAZGUL", '1d10', 4, 80, 500, 2, 20),
 
-(13, "PIRAÑA GIGANTE", '1d4', 1, 25,15, 1, 20),
-(14, "SERPIENTE DEL PANTANO", '1d6', 2, 35, 25, 1, 20),
-(15, "TIBURÓN BLANCO", '1d8',3, 60, 50, 2, 20),
-(16, "KRAKEN", '1d10', 4, 80, 70, 1, 25),
+(13, "PIRAÑA GIGANTE", '1d4', 1, 25,150, 1, 20),
+(14, "SERPIENTE DEL PANTANO", '1d6', 2, 35, 250, 1, 20),
+(15, "TIBURÓN BLANCO", '1d8',3, 60, 500, 2, 20),
+(16, "KRAKEN", '1d10', 4, 80, 700, 1, 25),
 
-(17, "DRAGÓN FAÉRICO", '1d4', 1, 30, 20, 2, 25),
-(18, "MURCIÉLAGO GIGANTE", '1d6', 2, 40, 30, 3, 25),
-(19, "QUIMERA", '1d8', 3, 60, 60, 2, 25),
-(20, "WYRM", '1d10', 4, 100, 80, 1, 30);
+(17, "DRAGÓN FAÉRICO", '1d4', 1, 30, 200, 2, 25),
+(18, "MURCIÉLAGO GIGANTE", '1d6', 2, 40, 300, 3, 25),
+(19, "QUIMERA", '1d8', 3, 60, 600, 2, 25),
+(20, "WYRM", '1d10', 4, 100, 800, 1, 30);
 
 /* Insertar armas */
 
 INSERT INTO `objeto` (`idObjeto`, `nombreObjeto`, `descripcionObjeto`, `estadistica1`, `valor1`, `estadistica2`, `valor2`, `precio`) VALUES
-(1, "MAZA DE DOS MANOS", "MAZA DE DOS MANOS", "fuerza", 10, NULL, NULL, 10),
-(2, "MAZA Y ESCUDO", "MAZA Y ESCUDO","constitucion", 10, NULL, NULL, 10),
-(3, "ESPADÓN", "ESPADÓN", "destreza", 10, NULL, NULL, 15),
-(4, "BASTÓN MÁGICO", "BASTON MAGICO", "inteligencia", 15, NULL, NULL, 15),
-(5, "MANGUAL", "MANGUAL", "fuerza", 5, "destreza", 5, 15),
-(6, "GRAN HACHA", "GRAN HACHA", "fuerza", 15, NULL, NULL, 10),
-(7, "ARCO LARGO", "ARCO LARGO", "destreza", 15, NULL, NULL, 15),
-(8, "GRAN BALLESTA", "GRAN BALLESTA", "fuerza", 30, "destreza", -5, 20),
-(9, "DAGA MÁGICA", "DAGA MÁGICA", "inteligencia", 10, NULL, NULL, 20),
-(10, "GRAN VARITA", "GRAN VARITA", "inteligencia", 30, "constitucion", -5, 25),
-(11, "GRAN ESCUDO", "GRAN ESCUDO", "fuerza", -5, "constitucion", 30, 30);
+(1, "MAZA DE DOS MANOS", "MAZA DE DOS MANOS", "fuerza", 2, NULL, NULL, 10),
+(2, "MAZA Y ESCUDO", "MAZA Y ESCUDO","fuerza", 2, "acPersonaje", 2, 10),
+(3, "ESPADÓN", "ESPADÓN", "fuerza", 6, NULL, NULL, 15),
+(4, "BASTÓN MÁGICO", "BASTON MAGICO", "inteligencia", 6, NULL, NULL, 15),
+(5, "MANGUAL", "MANGUAL", "fuerza", 4, "inteligencia", 4, 15),
+(6, "GRAN HACHA", "GRAN HACHA", "fuerza", 4, NULL, NULL, 10),
+(7, "ARCO LARGO", "ARCO LARGO", "destreza", 4, NULL, NULL, 15),
+(8, "GRAN BALLESTA", "GRAN BALLESTA", "destreza", 6, NULL, NULL, 20),
+(9, "DAGA MÁGICA", "DAGA MÁGICA", "inteligencia", 4, "destreza", 4, 20),
+(10, "GRAN VARITA", "GRAN VARITA", "inteligencia", 2, NULL, NULL, 25),
+(11, "ESPADA Y GRAN ESCUDO", "GRAN ESCUDO", "fuerza", 2, "acPersonaje", 4, 30),
+(24, "DAGA ARROJADIZA", "DAGA ARROJADIZA", "destreza", 2, "fuerza", 2, 30);
 
 /* Insertar armaduras */
 
@@ -55,11 +56,14 @@ INSERT INTO `objeto` (`idObjeto`, `nombreObjeto`, `descripcionObjeto`, `estadist
 /* Insertar pociones */
 
 INSERT INTO `objeto` (`idObjeto`, `nombreObjeto`, `descripcionObjeto`, `estadistica1`, `valor1`, `estadistica2`, `valor2`, `precio`) VALUES
-(19, "POCIÓN PEQUEÑA", "POCIÓN PEQUEÑA", "vidaActual", 5, NULL, NULL, 5),
-(20, "POCIÓN NORMAL", "POCIÓN NORMAL", "vidaActual", 10, NULL, NULL, 15),
-(21, "POCIÓN GRANDE", "POCIÓN GRANDE", "vidaActual", 15, NULL, NULL, 18),
-(22, "SUPERPOCIÓN", "SUPERPOCIÓN", "vidaActual", 25, NULL, NULL, 20),
-(23, "POCIÓN MÁXIMA", "POCIÓN MÁXIMA", "vidaActual", 40, NULL, NULL, 30);
+(19, "POCIÓN PEQUEÑA", "POCIÓN PEQUEÑA", "vidaActual", 10, NULL, NULL, 5),
+(20, "POCIÓN NORMAL", "POCIÓN NORMAL", "vidaActual", 20, NULL, NULL, 15),
+(21, "POCIÓN GRANDE", "POCIÓN GRANDE", "vidaActual", 40, NULL, NULL, 18),
+(22, "SUPERPOCIÓN", "SUPERPOCIÓN", "vidaActual", 60, NULL, NULL, 20),
+(23, "POCIÓN MÁXIMA", "POCIÓN MÁXIMA", "vidaActual", 100, NULL, NULL, 30),
+(25, "POCIÓN DE FUERZA DE TORO", "POCIÓN FUERZA DE TORO", "fuerza", 4, NULL, NULL, 30),
+(26, "POCIÓN DE AGILIDAD DE GATO", "POCIÓN DE AGILIDAD DE GATO", "destreza", 4, NULL, NULL, 30),
+(27, "POCIÓN DE SABIDURIA DE BUHO", "POCIÓN DE SABIDURIA DE BUHO", "inteligencia", 4, NULL, NULL, 30);
 
 /* Insertar tipos */
 
@@ -93,4 +97,8 @@ INSERT INTO `objetoTipo` (`idObjeto`, `idTipo`) VALUES
 (20, 3),
 (21, 3),
 (22, 3),
-(23, 3);
+(23, 3),
+(24, 1),
+(25, 3),
+(26, 3),
+(27, 3);
--- inserts-razas_clases_habilidades.sql
+++ inserts-razas_clases_habilidades.sql
@@ -20,12 +20,12 @@ INSERT INTO `habilidad` (`idHabilidad`, `nombreHabilidad`, `descripcionHabilidad
 (1, 'Colleja infernal', 'Concentra todo tu poder en la palma de la mano para asestar un golpe en la nuca.', '1d6', 'fuerza', 2),
 (2, 'Patada sencillita', 'Haz aspavientos innecesarios para dar una patada poco vistosa.', '1d8', 'fuerza', 1),
 (3, 'Golpe de trasero metalizado', 'Aprieta fuerte tus nalgas y golpea de espaldas.', '1d4', 'fuerza', 3),
-(4, 'Giro con doble tirabuzón hacia atrás', 'Gira rápidamente y presume de tus años compitiendo en salto sincronizado desde trampolín.', '2d6', 'fuerza', 0),
+(4, 'Locura transitoria', 'Sufres un periodo de enagenación mental transitoria para atacar indiscriminadamente.', '2d6', 'fuerza', 0),
 
 -- Sacerdote
 (5, 'Recitar la Biblia', 'Recita un fragmento de la Biblia para aturdir a tu oponente.', '2d4', 'inteligencia', 1),
 (6, 'Canción de cuna', 'Duerme a tu oponente con canciones de misa.', '1d10', 'inteligencia', 0),
-(7, 'Cruz malintencionada', 'Clava a tu oponente la cruz que llevas colgada del cuello.', '1d6', 'inteligencia', 3),
+(7, 'Cruz malintencionada', 'Clava a tu oponente la cruz que llevas colgada del cuello.', '1d6', 'fuerza', 3),
 (8, 'Enseñar patita por debajo de la falda', 'Haz insinuaciones impuras para despistar a tu oponente.', '1d8', 'inteligencia', 2),
 
 -- Mago
@@ -35,16 +35,16 @@ INSERT INTO `habilidad` (`idHabilidad`, `nombreHabilidad`, `descripcionHabilidad
 (12, 'Al microondas', 'Un minuto y las entrañas de tu oponente irán explotando como palomitas en el microondas.', '3d4', 'inteligencia', 0),
 
 -- Nigromante
-(13, 'Mordisco a lo Walking Dead', 'Infecta a tu oponente con un virus zombie.', '1d4', 'fuerza', 3),
-(14, 'Lapidazo', 'Arroja la lápida de uno de tus antepasados que llevas guardada en el zurrón.', '1d12', 'fuerza', 1),
-(15, 'Bola sombra de suricato satánico', 'Concentra y lanza una bola de energía contra tu oponente.', '2d4', 'fuerza', 2),
-(16, 'Extraer alma a ostias', 'Usa tus poderes de golpeo para absorber el alma de tu oponente.', '2d6', 'fuerza', 0),
+(13, 'Mordisco a lo Walking Dead', 'Infecta a tu oponente con un virus zombie.', '1d4', 'destreza', 3),
+(14, 'Lapidazo', 'Arroja la lápida de uno de tus antepasados que llevas guardada en el zurrón.', '1d12', 'destreza', 1),
+(15, 'Bola sombra de suricato satánico', 'Concentra y lanza una bola de energía contra tu oponente.', '2d4', 'inteligencia', 2),
+(16, 'Extraer alma a ostias', 'Usa tus poderes de golpeo para absorber el alma de tu oponente.', '2d6', 'destreza', 0),
 
 -- Cazador
-(17, 'Golpe a los huevos', 'Asesta un golpe bajo donde más duele.', '1d4', 'destreza', 3),
+(17, 'Golpe a los huevos', 'Asesta un golpe bajo donde más duele.', '1d4', 'fuerza', 3),
 (18, 'Corbata colombiana', 'Rodea a tu oponente y hazle una llave.', '1d8', 'destreza', 2),
 (19, 'Trampa para ratones', 'Haz que tu oponente caiga en una trampa que lo inmovilice.', '1d10', 'destreza', 1),
-(20, 'Locura transitoria', 'Sufres un periodo de enagenación mental transitoria para atacar indiscriminadamente.', '2d6', 'destreza', 0);
+(20, 'Giro con doble tirabuzón hacia atrás', 'Gira rápidamente y presume de tus años compitiendo en salto sincronizado desde trampolín.', '2d6', 'destreza', 0);
 
 --
 -- Relaciones entre `habilidad` y `habilidadclase`
