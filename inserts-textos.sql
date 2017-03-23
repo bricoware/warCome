5f02c8c453cba193e281263bfef3a4e76c4db7e5
sgaeba <gabrielpenide@gmail.com>
2017-03-23 13:27:23 +0100

    Primera versión de la inserción de textos

---
+++ inserts-textos.sql
@@ -0,0 +1,139 @@
+/* Inserción de textos */
+
+INSERT INTO `texto` (`idTexto`, `texto`) VALUES
+(1, '&lt;div id=&quot;caja1&quot;&gt;
+		&lt;div id=&quot;introduccion&quot; class=&quot;balloon&quot; &gt;
+			&lt;p&gt;Permitidme que os cuente mi historia.&lt;/p&gt;
+			&lt;p&gt;Me crié en una pequeña casa en el bosque, apartado de todo conflicto, protegido por un encantamiento que mi abuelo había creado, siempre le preguntaba
+			 el motivo de el encantamiento y por qué no había nadie más aquí; a lo que él me respondía -Esta tierra esconde grandes peligros, antes de salir
+			 de aquí, necesitas aprender ciertas cosas, el futuro te depara grandes cosas, grandes batallas, tu nombre será conocido en todo el mundo pero otros
+			 te recordarán por ser la &quot;Profecía&quot;, eres la luz, la esperanza.&lt;/p&gt;
+			&lt;p&gt; Desde niño sabía cual era mi destino.&lt;/p&gt;
+			&lt;p&gt;Antes de que llegase este periodo de paz a esta tierra, fue sometido a una fuerza sin igual, nadie había visto ni sentido nada igual.
+			 Todos los Reyes de este reino reunieron a sus mejores guerreros para combatirlo, llamaron a los más grandes magos de todos los tiempos y ni así
+			 se acercarón a su reino.&lt;/p&gt;
+			 &lt;p&gt;Pero un día un druida reunión a todos los Reyes del reino para contarles una profecía que se cumpliría 20 años más tarde, la profecía era la siguiente:&lt;/p&gt;&lt;/div&gt;
+											
+											&lt;div id=&quot;profecia&quot;&gt;&lt;p&gt;&quot;Solo un ser de puro corazón, portador de la luz, será el elegido.
+											 No subestiméis su aspecto ya que su poder proviene de su corazón.
+											 Cuando sus fuerzas flaqueen su corazón le mostrará el camino y
+											 liberará esta tierra de las entrañas de la oscuridad.&quot;&lt;/p&gt;&lt;/div&gt;
+											 
+			&lt;div id=&quot;introduccion1&quot;&gt;&lt;p&gt;Todos los presentes celebraron esas palabras pero esperar 20 años sería duro. Fueron pasando los años, el agua y la comida era cada vez más escasa,
+			 muchos de los reinos habían desaparecido pero los que quedaban recordaban las palabras del druida, pasados los 20 años solo quedaba un Rey,
+			 el Señor de El Bosque Encantado, lo llamaban así por el cuidado que le daba a los bosques y a su envidiable jardín; la gente del reino dicen que
+			 Ésus &quot;Dios de los árboles y protector de los bosques&quot; le había dado un don.&lt;/p&gt;
+			 &lt;p&gt;Un día, ya desesperado, mandó a que buscasen a la &quot;Profecía&quot;...&lt;/p&gt;
+			 
+			&lt;p&gt;Y aquí es donde empieza mi aventura en esta extraña tierra llena de peligros y seres increíbles.&lt;/p&gt;&lt;/div&gt;
+		&lt;/div&gt;
+&lt;/div&gt;'),
+(2, '&lt;div id=&quot;primeraZona&quot;&gt;
+	&lt;div id=&quot;castillo&quot; onclick=&quot;historia();&quot;&gt;
+		&lt;p&gt;Me presenté delante del Rey, en su expresión se podía distinguir sorpresa y esperanza.&lt;/p&gt;
+	&lt;/div&gt;
+	&lt;div id=&quot;dialogo&quot; class=&quot;balloon&quot;&gt;
+		&lt;p&gt;(Rey)-Ahora las palabras de ese viejo druida toman sentido.
+  		    Por favor jovén, ayudanos con esta situación, esta tierra se consume cada día más y mi pueblo cada día disminuye,
+		    bien por la mala allimentación o ya sea porque sucumben al poder de ese &quot;REY DEL MAL&quot;.
+			Te daré cualquier cosa que pidas pero te lo suplico, AYÚDANOS.
+		&lt;/p&gt;
+	&lt;/div&gt;
+
+	&lt;div id=&quot;foso&quot;&gt;
+
+		&lt;div id=&quot;dialogo1&quot;&gt;
+			&lt;p&gt;Debo darme prisa, los engendros de la oscuridad ya se estan acercando.
+			   VOLVER A LA OSCURIDAD ENGENDROS DEL MAL!!!
+			&lt;/p&gt;
+
+		&lt;div id=&quot;pensamientos0&quot;&gt;
+			&lt;p&gt;A medida que iban pasando las horas y me iba alejando de El Bosque Encantado, podía apreciar como el paso de los años bajo el yugo de 
+			   El Rey Del Mal no era de extrañar, solo viendo esa pequeña muestra, que grandes ejércitos sucumbieran ante él; recuerdo que mi abuelo me contaba
+			   que hasta el propio Lucifer le temía y le otorgó ciertos dones que estaban prohibidos utilizar, le concedió el poder de la nigromancia 
+			   pero tal era su poder que no se conformó con liderar a ejércitos de NO MUERTOS, fue más allá, se adentró en las profundidades más 
+			   oscuras y se hizo con el mayor y temible ejército nunca antes visto por el hombre ni por ninguna otra criatura, solo nos podíamos 
+			   imaginar todos esos terrores por lo contado por Dante en su viaje por el purgatorio.
+		       La sensación de inseguridad estaba presente desde que puse el pie fuera del Castillo y de su protección, los gemidos deseosos de esos
+			   seres por alimentarse de mi corazón y de mi alma era más que evidente; pobres criaturas.
+			&lt;/p&gt;
+		&lt;/div&gt;
+
+&lt;/div&gt;'),
+(3, '&lt;div id=&quot;segundaZona&quot;&gt;
+	&lt;div id=&quot;jardines&quot; onclick=&quot;historia();&quot;&gt;
+		&lt;p&gt;Era un inmenso jardín, precioso, en su día tuvo que ser precioso y muy bien cuidado (las historias contadas en el pueblo deberían ser ciertas).
+		   Ahora el jardín ya no lucia como antaño, ahora todo eran zarzas, maleza y restos de ceniza; hasta la propia naturaleza sumcumbió a tal poder.
+		&lt;/p&gt;
+	&lt;/div&gt;
+	&lt;div id=&quot;decisiones&quot;&gt;
+		&lt;p&gt;El destino es caprichoso, ante mis ojos dos caminos. ¿Cúal sería el correcto? ¿Cúal más seguro? ¿Cúal me llevaría a mi destino?
+	      ¿Serían reales? ¿Una ilusión creada por el propio mal?
+	       No tenía más remedio que cruzar uno, pero ¿cúal?...
+		&lt;/p&gt;
+	&lt;/div&gt;	
+
+&lt;/div&gt;'),
+(4, '&lt;div id=&quot;terceraZonaA&quot;&gt;
+	&lt;div id=&quot;pensamientos1&quot;&gt;
+		&lt;p&gt;Se notaba que estaba rodeado, tenía la sensación de que hasta las zarzas y la maleza atacarían en cualquier momento...
+		&lt;/p&gt;
+	&lt;/div&gt;
+&lt;/div&gt;'),
+(5, '&lt;div id=&quot;terceraZonaB&quot;&gt;
+	&lt;div id=&quot;pensamientos2&quot; &gt;
+		&lt;p&gt;Había un puente en estado dudoso, pero era normal después de tantos años y tantos daños.
+		&lt;/p&gt;
+	&lt;/div&gt;
+&lt;/div&gt;'),
+(6, '&lt;div id=&quot;cuartaZona&quot;&gt;
+	&lt;div id=&quot;pensamientos3&quot;&gt;
+		&lt;p&gt;Seguí mi camino, era consciente de lo poco que quedaba para el principio del fin. El camino era cada vez más peligroso, oscuro; la desesperación,
+		   la ira, el terror estaban más presentes. Al rato de seguir el sendero pude ver una cueva, con la entrada sin vigilancia... o eso creía
+		&lt;/p&gt;
+	&lt;/div&gt;
+
+	&lt;div id=&quot;pensamientos4&quot;&gt;
+		&lt;p&gt;Las leyendas tenían razón, su poder era enorme, o debía serlo si tenía a su servicio a El Perro de Urco. Me adentré en la cueva, sigilosamente fui
+		   avanzando por esos pasillos labenrínticos, hasta llegar a una amplia sala en la que había un grupo de encapuchados recitando, ¿un conjuro?, sguramente.
+		   Me fijé un poco más en sus ropajes y me di cuenta de quienes eran, La Santa Compaña, intenté salir de esa sala sin ser detectado pero fue inútil,
+		   ya se habían percatado de mi presencia.
+		&lt;/p&gt;
+	&lt;/div&gt;
+
+	&lt;div id=&quot;pensamientos5&quot;&gt;
+		&lt;p&gt;Agotado de la batalla, decidí continuar, no había tiempo que perder, notaba su presencia cerca y él notaba la mía, estaba furioso, había derrotado a
+		   sus mejores y temibles hombres, su poder estaba en entre dicho.
+		   Al llegar a sus aposentos me di cuenta de que la cueva no era una cueva normal, era la entrada al infierno.
+		   Era la hora de la batalla contra El Señor De Todo Mal.
+		   Que los dioses nos asistan.
+		&lt;/p&gt;
+	&lt;/div&gt;
+	&lt;div id=&quot;declaracion&quot;&gt;
+		&lt;p&gt;DEVOLVERÉ LA PAZ Y LA LUZ A ESTA TIERRA!!! 
+		
+		   ACABARÉ CONTIGO Y VOLVERÁS A ESE POZO DE PUTEFRACCIÓN DEL QUE SALISTE!!!
+		   
+		   NUNCA PODRÁS DERROTAR LA LUZ DE ESTE MUNDO!!!
+		   
+		   BRIGHID &quot;DIOSA DE LA CURACIÓN&quot; TE IMPLORO AYUDA PARA DESTERRAR A ESTE SER Y DEVOLVERTE TU PODER!!!
+		   
+		   ÉSUS &quot;DIOS DE LOS ÁRBOLES Y PROTECTOR DE LOS BOSQUES&quot; PRÉSTAME TU PODER PARA LIBERAR A TU CREACIÓN DE LAS GARRAS DE LAS TINIEBLAS DE ESTE SER!!!
+		   
+		   TEUTATES &quot;DIOS DEL PUEBLO&quot; ME ENCOMIENDO A TI PARA HACER TU VOLUNTAD Y LIBERAR A TU CREACIÓN!!!
+			 
+				ARDERÁS EN LAS LLAMAS DEL PURGATORIO!!!!
+		&lt;/p&gt;
+	&lt;/div&gt;
+
+&lt;/div&gt;');
+
+/* Relaciones entre `texto` y `aventura` */
+
+INSERT INTO `aventura` (`idDificultad`, `idTexto`, `idZona`, `imagenAventura`) VALUES
+(1, 1, 0, "placeholder-para-ruta"),
+(1, 2, 1, "placeholder-para-ruta"),
+(1, 3, 2, "placeholder-para-ruta"),
+(1, 4, 3, "placeholder-para-ruta"),
+(1, 5, 3, "placeholder-para-ruta"),
+(1, 6, 4, "placeholder-para-ruta");
