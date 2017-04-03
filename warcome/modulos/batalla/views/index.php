21cf625fc4dc4e778aaa7b02b6d56e599791e9fc
Cramsi121 <cramsi121@gmail.com>
2017-04-03 09:01:47 +0200

    batalla links css, js

--- warcome/modulos/batalla/views/index.php
+++ warcome/modulos/batalla/views/index.php
@@ -7,6 +7,6 @@
 <!-- El botón está solo para testeos -->
 <!-- <input type="button" onclick="Javascript:comenzarBatalla(1);" value="Descomentar para testeos" /> -->
 
-<link rel="stylesheet" type="text/css" href="views/css/batalla.css"/>
-<script type="text/javascript" src="views/js/dados.js"></script>
-<script type="text/javascript" src="views/js/batalla.js"></script>
+<link rel="stylesheet" type="text/css" href="/modulos/batalla/views/css/batalla.css"/>
+<script type="text/javascript" src="/modulos/batalla/views/js/dados.js"></script>
+<script type="text/javascript" src="/modulos/batalla/views/js/batalla.js"></script>
